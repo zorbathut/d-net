d3d76c0743d57440c856f34327a79b7e76d27ad3
Ben Wilhelm <zorba-git20110501@pavlovian.net>
2006-09-13 13:59:11 +0000

    A little reorganization

--- metagame_tween.h
+++ metagame_tween.h
@@ -5,44 +5,55 @@
 #include "shop.h"
 
 class PersistentData {
+public:
+  // Player data and reorganization functions
+  vector<Player> &players();
+
+  vector<Keystates> genKeystates(const vector<Controller> &keys) const;
+
+  void ai(const vector<Ai *> &ais) const;
+  vector<Ai *> distillAi(const vector<Ai *> &ais) const;
+
+  // State accessors
+  bool isPlayerChoose() const;
+
+  // Main loop
+  bool tick(const vector< Controller > &keys);
+  void render() const;
+
+  // Triggers
+  void divvyCash(float firepowerSpent);
+
+  // Constructor
+  PersistentData(int playercount, int roundsbetweenshop);
+
+private:
+  // Persistent state (used for recognizing PLAYERCHOOSE mode only)
   enum { TM_PLAYERCHOOSE, TM_SHOP };
   int mode;
   
+  // Player data
   vector<Player> playerdata;
-    
-  Shop shop;
-  int currentShop;
-  
   vector<PlayerMenuState> pms;  // heh.
   
   vector<FactionState> factions;
   
+  // Temporaries for scoring
   vector<vector<float> > lrCategory;
   vector<float> lrPlayer;
   vector<Money> lrCash;
   vector<bool> checked;
   
+  // Tween layout info
+  Shop shop;
+  int currentShop;
+  
+  // Round count data
   int roundsbetweenshop;
   int shopcycles;
-  
-public:
-  bool isPlayerChoose() const;
 
-  vector<Player> &players();
-
-  vector<Keystates> genKeystates(const vector<Controller> &keys) const;
-
-  bool tick(const vector< Controller > &keys);
-  void render() const;
-
-  void ai(const vector<Ai *> &ais) const;
-  vector<Ai *> distillAi(const vector<Ai *> &ais) const;
-
-  void divvyCash(float firepowerSpent);
-
+  // Helper functions
   void drawMultibar(const vector<float> &sizes, const Float4 &dimensions) const;
-
-  PersistentData(int playercount, int roundsbetweenshop);
 };
 
 #endif
