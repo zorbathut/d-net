ea3816a1834e10d6a8cf9e6875c384264df8bbac
Ben Wilhelm <zorba-git20110501@pavlovian.net>
2006-12-23 16:24:18 +0000

    Smooth rendering seems to kind of work

--- vecedit2.cpp
+++ vecedit2.cpp
@@ -2,6 +2,8 @@
 #include <wx/wx.h>
 #include <wx/glcanvas.h>
 
+#include "debug.h"
+
 class MyApp: public wxApp {
   virtual bool OnInit();
 };
@@ -16,6 +18,8 @@ public:
   void OnQuit(wxCommandEvent& event);
   void OnAbout(wxCommandEvent& event);
 
+  void OnPaint(wxPaintEvent& event);
+
   void render();
   
   DECLARE_EVENT_TABLE()
@@ -29,6 +33,7 @@ enum {
 BEGIN_EVENT_TABLE(MyFrame, wxFrame)
 EVT_MENU(ID_Quit, MyFrame::OnQuit)
 EVT_MENU(ID_About, MyFrame::OnAbout)
+EVT_PAINT(MyFrame::OnPaint)
 END_EVENT_TABLE()
 
 IMPLEMENT_APP(MyApp)
@@ -81,19 +86,34 @@ void MyFrame::OnAbout(wxCommandEvent& WXUNUSED(event)) {
   wxMessageBox("This is a level editor.", "About D-Net Vecedit2", wxOK | wxICON_INFORMATION, this);
 }
 
+void MyFrame::OnPaint(wxPaintEvent& event) {
+  wxPaintDC dc(this);
+  
+  render();
+}
+
 void MyFrame::render() {
   canvas->SetCurrent();
+  
+  static float foo = 0.3;
+  foo += 0.1;
+  if(foo > 0.9)
+    foo = 0.1;
 
-  wxSafeYield();
+  dprintf("render, %f\n", foo);
+  //wxSafeYield();
   glClearColor(0.0, 0.0, 0.0, 0.0);
-  //glViewport(0, 0, (GLint)200, (GLint)200);
+  glClearStencil(0);
+  glClear(GL_COLOR_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+  CHECK(glGetError() == GL_NO_ERROR);
+  
   glColor3f(1.0, 1.0, 1.0);
   
   glBegin(GL_POLYGON);
-    glVertex2f(-0.5, -0.5);
-    glVertex2f(-0.5, 0.5);
-    glVertex2f(0.5, 0.5);
-    glVertex2f(0.5, -0.5);
+    glVertex2f(-foo, -foo);
+    glVertex2f(-foo, foo);
+    glVertex2f(foo, foo);
+    glVertex2f(foo, -foo);
   glEnd();
   glFlush();
   
