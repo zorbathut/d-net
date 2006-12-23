688f4d44d411401cdf5ce24582b434535415b7df
Ben Wilhelm <zorba-git20110501@pavlovian.net>
2006-12-23 16:37:45 +0000

    Structure things a bit better

--- vecedit2.cpp
+++ vecedit2.cpp
@@ -8,19 +8,15 @@ class MyApp: public wxApp {
   virtual bool OnInit();
 };
 
+IMPLEMENT_APP(MyApp)
+
 class MyFrame : public wxFrame {
-private:
-  wxGLCanvas *canvas;
 public:
   
   MyFrame(const wxString& title);
   
   void OnQuit(wxCommandEvent& event);
   void OnAbout(wxCommandEvent& event);
-
-  void OnPaint(wxPaintEvent& event);
-
-  void render();
   
   DECLARE_EVENT_TABLE()
 };
@@ -33,18 +29,26 @@ enum {
 BEGIN_EVENT_TABLE(MyFrame, wxFrame)
 EVT_MENU(ID_Quit, MyFrame::OnQuit)
 EVT_MENU(ID_About, MyFrame::OnAbout)
-EVT_PAINT(MyFrame::OnPaint)
 END_EVENT_TABLE()
 
-IMPLEMENT_APP(MyApp)
+class MyGLC : public wxGLCanvas {
+public:
+  
+  MyGLC(wxWindow *wind);
+  
+  void OnPaint(wxPaintEvent& event);
+
+  DECLARE_EVENT_TABLE()
+};
+
+BEGIN_EVENT_TABLE(MyGLC, wxGLCanvas)
+EVT_PAINT(MyGLC::OnPaint)
+END_EVENT_TABLE()
 
 bool MyApp::OnInit() {
   MyFrame *frame = new MyFrame("D-Net Vecedit2");
   frame->Show(TRUE);
   SetTopWindow(frame);
-  
-  frame->render();
-  
   return TRUE;
 }
 
@@ -68,14 +72,11 @@ MyFrame::MyFrame(const wxString& title) : wxFrame((wxFrame *)NULL, -1, title) {
   }
   
   SetMenuBar( menuBar );
-  
-  wxPanel *panel = new wxPanel(this);
 
-  canvas = new wxGLCanvas(panel, -1, wxDefaultPosition, wxDefaultSize);
-  canvas->SetSize(GetClientSize());
+  new MyGLC(this);
   
   CreateStatusBar();
-  SetStatusText( "Welcome to wxWidgets!" );
+  SetStatusText("borf borf borf");
 }
 
 void MyFrame::OnQuit(wxCommandEvent& WXUNUSED(event)) {
@@ -86,21 +87,21 @@ void MyFrame::OnAbout(wxCommandEvent& WXUNUSED(event)) {
   wxMessageBox("This is a level editor.", "About D-Net Vecedit2", wxOK | wxICON_INFORMATION, this);
 }
 
-void MyFrame::OnPaint(wxPaintEvent& event) {
+MyGLC::MyGLC(wxWindow *wind) : wxGLCanvas(wind, -1, wxDefaultPosition, wxDefaultSize) { };
+
+void MyGLC::OnPaint(wxPaintEvent& event) {
   wxPaintDC dc(this);
   
-  render();
-}
-
-void MyFrame::render() {
-  canvas->SetCurrent();
+  SetCurrent();
   
   static float foo = 0.3;
   foo += 0.1;
-  if(foo > 0.9)
+  if(foo > 3.0)
     foo = 0.1;
 
   dprintf("render, %f\n", foo);
+  dprintf("size is %d,%d\n", GetSize().x, GetSize().y);
+  
   //wxSafeYield();
   glClearColor(0.0, 0.0, 0.0, 0.0);
   glClearStencil(0);
@@ -117,5 +118,5 @@ void MyFrame::render() {
   glEnd();
   glFlush();
   
-  canvas->SwapBuffers();
-};
+  SwapBuffers();
+}
