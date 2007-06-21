04c49b6516e0babd0d15d6d88c91843eee6068e4
Ben Wilhelm <zorba-git20110501@pavlovian.net>
2007-06-21 05:40:49 +0000

    Add framework for ods2csv

---
+++ ods2csv.cpp
@@ -0,0 +1,16 @@
+
+#include "debug.h"
+
+#include <xercesc/util/PlatformUtils.hpp>
+#include <xercesc/dom/DOMDocument.hpp>
+#include <xercesc/parsers/XercesDOMParser.hpp>
+#include <xercesc/framework/LocalFileFormatTarget.hpp>
+
+using namespace xercesc;
+
+int main() {
+  XMLPlatformUtils::Initialize();
+  
+  dprintf("lolz\n");
+  XMLPlatformUtils::Terminate();
+}
