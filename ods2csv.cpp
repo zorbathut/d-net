c9bc12db2fd34bfa9ccacacb370bddb3182c0a31
Ben Wilhelm <zorba-git20110501@pavlovian.net>
2007-06-21 07:00:39 +0000

    Update minizip, now opens the zip file

--- ods2csv.cpp
+++ ods2csv.cpp
@@ -1,16 +1,30 @@
 
 #include "debug.h"
+#include "os.h"
+
+#include "minizip/unzip.h"
 
 #include <xercesc/util/PlatformUtils.hpp>
 #include <xercesc/dom/DOMDocument.hpp>
 #include <xercesc/parsers/XercesDOMParser.hpp>
 #include <xercesc/framework/LocalFileFormatTarget.hpp>
 
+using namespace std;
 using namespace xercesc;
 
-int main() {
+int main(int argc, char *argv[]) {
+  set_exename("ods2csv.exe");
+  
+  CHECK(argc == 4);
+  
+  unzFile unzf = unzOpen(argv[2]);
+  CHECK(unzf);
+  
   XMLPlatformUtils::Initialize();
   
-  dprintf("lolz\n");
   XMLPlatformUtils::Terminate();
+  
+  unzClose(unzf);
+
+  CHECK(0);
 }
