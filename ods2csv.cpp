9ab23475a59bc87b5c83ca0bb9f8dd1ff0192b96
Ben Wilhelm <zorba-git20110501@pavlovian.net>
2007-06-23 00:23:39 +0000

    gradual progress

--- ods2csv.cpp
+++ ods2csv.cpp
@@ -6,6 +6,7 @@
 
 #include <xercesc/util/PlatformUtils.hpp>
 #include <xercesc/dom/DOMDocument.hpp>
+#include <xercesc/dom/DOMNamedNodeMap.hpp>
 #include <xercesc/dom/DOMTreeWalker.hpp>
 #include <xercesc/framework/MemBufInputSource.hpp>
 #include <xercesc/parsers/XercesDOMParser.hpp>
@@ -15,12 +16,96 @@
 using namespace std;
 using namespace xercesc;
 
-inline string FX( const XMLCh *d ) {
-	char *dat = XMLString::transcode( d );
+class XSargh {
+public :
+  
+  const XMLCh* get() const { return xs; }
+  
+  XSargh(const char *in) { xs = XMLString::transcode(in); }
+	XSargh(const string &in) { xs = XMLString::transcode(in.c_str()); }
+
+  ~XSargh() { XMLString::release(&xs); }
+
+private :
+  XMLCh*   xs;
+};
+
+#define X(str) XSargh(str).get()
+
+string FX(const XMLCh *d) {
+	char *dat = XMLString::transcode(d);
 	string out = dat;
-	XMLString::release( &dat );
+	XMLString::release(&dat);
 	return out;
 };
+
+string attrib(DOMTreeWalker *walkr, const string &attr) {
+  DOMNamedNodeMap *mp = walkr->getCurrentNode()->getAttributes();
+  CHECK(mp);
+  DOMNode *n = mp->getNamedItem(X(attr));
+  CHECK(n);
+  return FX(n->getNodeValue());
+};
+
+vector<vector<string> > extractDataFromTable(DOMTreeWalker *walkor) {
+  CHECK(FX(walkor->getCurrentNode()->getNodeName()) == "table:table");
+  CHECK(walkor->firstChild());
+  CHECK(walkor->parentNode());
+  
+  vector<vector<string> > rv;
+  
+  return rv;
+}
+
+vector<vector<string> > extractData(const vector<unsigned char> &data, const string &tablename) {
+  XMLPlatformUtils::Initialize();
+  
+  vector<vector<string> > rv;
+  
+  {
+    MemBufInputSource mbis(&data[0], data.size(), "extracted_data");
+    XercesDOMParser parse;
+    parse.parse(mbis);
+    
+    dprintf("Content parsed\n");
+    
+    DOMDocument *doc = parse.getDocument();
+    DOMTreeWalker *walkor = doc->createTreeWalker(doc, DOMNodeFilter::SHOW_ALL, NULL, true);
+    
+    CHECK(walkor->firstChild());
+    CHECK(FX(walkor->getCurrentNode()->getNodeName()) == "office:document-content");
+    CHECK(walkor->firstChild());
+    
+    bool foundbody = false;
+    do {
+      if(FX(walkor->getCurrentNode()->getNodeName()) == "office:body") {
+        CHECK(!foundbody);
+        foundbody = true;
+        CHECK(walkor->firstChild());
+        CHECK(FX(walkor->getCurrentNode()->getNodeName()) == "office:spreadsheet");
+        CHECK(walkor->firstChild());
+        do {
+          CHECK(FX(walkor->getCurrentNode()->getNodeName()) == "table:table");
+          if(attrib(walkor, "table:name") == tablename) {
+            CHECK(rv.empty());
+            rv = extractDataFromTable(walkor);
+            CHECK(!rv.empty());
+          }
+        } while(walkor->nextSibling());
+        CHECK(walkor->parentNode());
+        CHECK(walkor->parentNode());
+      }
+    } while(walkor->nextSibling());
+    CHECK(foundbody);
+    dprintf("prerelease\n");
+    walkor->release();
+    //doc->release();  // enabling this makes it crash. why? because xerces is weird
+  }
+  
+  XMLPlatformUtils::Terminate();
+  
+  CHECK(0);
+}
 
 int main(int argc, char *argv[]) {
   set_exename("ods2csv.exe");
@@ -49,48 +134,18 @@ int main(int argc, char *argv[]) {
     dprintf("Content read, %d bytes\n", file.size());
   }
   
-  XMLPlatformUtils::Initialize();
-  
-  {
-    MemBufInputSource mbis(&file[0], file.size(), argv[2]);
-    XercesDOMParser parse;
-    parse.parse(mbis);
-    
-    dprintf("Content parsed\n");
-    
-    DOMDocument *doc = parse.getDocument();
-    DOMTreeWalker *walkor = doc->createTreeWalker(doc, DOMNode::ELEMENT_NODE, NULL, true);
-    
-    CHECK(walkor->firstChild());
-    CHECK(FX(walkor->getCurrentNode()->getNodeName()) == "office:document-content");
-    CHECK(walkor->firstChild());
-    
-    bool foundbody = false;
-    do {
-      if(FX(walkor->getCurrentNode()->getNodeName()) == "office:body") {
-        CHECK(!foundbody);
-        foundbody = true;
-        CHECK(walkor->firstChild());
-        CHECK(FX(walkor->getCurrentNode()->getNodeName()) == "office:spreadsheet");
-        CHECK(walkor->firstChild());
-        do {
-          CHECK(FX(walkor->getCurrentNode()->getNodeName()) == "table:table");
-          CHECK(walkor->firstChild());
-          
-          CHECK(walkor->parentNode());
-        } while(walkor->nextSibling());
-        CHECK(walkor->parentNode());
-        CHECK(walkor->parentNode());
-      }
-    } while(walkor->nextSibling());
-    CHECK(foundbody);
-    dprintf("prerelease\n");
-    walkor->release();
-    //doc->release();  // enabling this makes it crash. why? because xerces is weird
-  }
-  
-  XMLPlatformUtils::Terminate();
+  vector<vector<string> > dat = extractData(file, argv[3]);
 
-  dprintf("ENDING YOU AND ALL YOU LOVE\n");
-  CHECK(0);
+  {
+    FILE *of = fopen(argv[1], "wb");
+    for(int i = 0; i < dat.size(); i++) {
+      for(int j = 0; j < dat[i].size(); j++) {
+        if(j)
+          fprintf(of, ",");
+        fprintf(of, "\"%s\"", dat[i][j].c_str());
+      }
+      fprintf(of, "\n");
+    }
+    fclose(of);
+  }
 }
