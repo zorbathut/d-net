c92314a78b7dcbb4337eb96a8b73b35308185b69
Ben Wilhelm <zorba-git20110501@pavlovian.net>
2007-06-26 19:07:49 +0000

    Disable perfbar

--- perfbar.cpp
+++ perfbar.cpp
@@ -3,11 +3,14 @@
 
 #include "gfx.h"
 #include "timer.h"
+#include "args.h"
 
 #include <vector>
 #include <stack>
 
 using namespace std;
+
+DEFINE_bool(perfbar, "Enable performance bar", false);
 
 class PerfChunk {
 public:
@@ -24,37 +27,45 @@ static stack<pair<const PerfStack *, PerfChunk> > pstack;
 static Timer timer;
 
 PerfStack::PerfStack(const Color &col) {
-  PerfChunk pch;
-  pch.start = timer.ticksElapsed() / (float)timer.getFrameTicks();
-  pch.col = col;
-  pch.indent = pstack.size();
-  pstack.push(make_pair(this, pch));
+  if(FLAGS_perfbar) {
+    PerfChunk pch;
+    pch.start = timer.ticksElapsed() / (float)timer.getFrameTicks();
+    pch.col = col;
+    pch.indent = pstack.size();
+    pstack.push(make_pair(this, pch));
+  }
 }
 PerfStack::~PerfStack() {
-  CHECK(pstack.top().first == this);
-  PerfChunk pch = pstack.top().second;
-  pstack.pop();
-  pch.end = timer.ticksElapsed() / (float)timer.getFrameTicks();
-  pchunks.push_back(pch);
+  if(FLAGS_perfbar) {
+    CHECK(pstack.top().first == this);
+    PerfChunk pch = pstack.top().second;
+    pstack.pop();
+    pch.end = timer.ticksElapsed() / (float)timer.getFrameTicks();
+    pchunks.push_back(pch);
+  }
 }
 
 void startPerformanceBar() {
-  CHECK(pstack.empty());
-  pchunks.clear();
-  timer = Timer();
+  if(FLAGS_perfbar) {
+    CHECK(pstack.empty());
+    pchunks.clear();
+    timer = Timer();
+  }
 }
 
 void drawPerformanceBar() {
-  setZoomVertical(0, 0, 1.2);
-  const float xmarg = 0.02;
-  const float xstart = getZoom().ex - xmarg * 2;
-  int megindent = 0;
-  for(int i = 0; i < pchunks.size(); i++) {
-    float xps = xstart - xmarg * pchunks[i].indent;
-    setColor(pchunks[i].col);
-    drawLine(Float4(xps, pchunks[i].start, xps, pchunks[i].end), xmarg * 0.9);
-    megindent = max(megindent, pchunks[i].indent);
+  if(FLAGS_perfbar) {
+    setZoomVertical(0, 0, 1.2);
+    const float xmarg = 0.02;
+    const float xstart = getZoom().ex - xmarg * 2;
+    int megindent = 0;
+    for(int i = 0; i < pchunks.size(); i++) {
+      float xps = xstart - xmarg * pchunks[i].indent;
+      setColor(pchunks[i].col);
+      drawLine(Float4(xps, pchunks[i].start, xps, pchunks[i].end), xmarg * 0.9);
+      megindent = max(megindent, pchunks[i].indent);
+    }
+    setColor(C::gray(1.0));
+    drawLine(Float4(xstart - xmarg * (megindent + 2), 1.0, getZoom().ex, 1.0), xmarg / 2);
   }
-  setColor(C::gray(1.0));
-  drawLine(Float4(xstart - xmarg * (megindent + 2), 1.0, getZoom().ex, 1.0), xmarg / 2);
 }
