329f0bd9428d86aa49e3dfe0dee885651ee6d648
Ben Wilhelm <zorba-git20110501@pavlovian.net>
2007-09-25 00:12:17 +0000

    Add timewarp keys

--- core.cpp
+++ core.cpp
@@ -32,6 +32,7 @@ DEFINE_int(randomizeFrameRender, 0, "Randomize frame render change to 1/this (de
 DEFINE_bool(frameskip, true, "Enable or disable frameskipping");
 DEFINE_bool(render, true, "Render shit");
 DEFINE_bool(timing, true, "Display timing information");
+DEFINE_bool(warpkeys, false, "Enable timewarp keys");
 
 void displayCZInfo();
 
@@ -73,14 +74,18 @@ void MainLoop() {
 
   Timer bencher;
   
+  int speed = 1;
+  
   while(!quit) {
+    bool thistick = false;
+    
     startPerformanceBar();
     if(FLAGS_timing)
       bencher = Timer();
     StackString sst(StringPrintf("Frame %d loop", frameNumber));
     tickHttpd();
     ffwd = (frameNumber < FLAGS_fastForwardTo);
-    if(frameNumber == FLAGS_fastForwardTo)
+    if(frameNumber == FLAGS_fastForwardTo || speed != 1)
       timer = Timer();    // so we don't end up sitting there for aeons waiting for another frame
     if(FLAGS_timing)
       bencher = Timer();
@@ -95,6 +100,16 @@ void MainLoop() {
         case SDL_KEYUP:
           if(event.key.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE)
             quit = true;
+          
+          if(FLAGS_warpkeys && event.key.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_z)
+            speed = !speed;
+          if(FLAGS_warpkeys && event.key.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_x)
+            speed = 10;
+          if(FLAGS_warpkeys && event.key.type == SDL_KEYUP && event.key.keysym.sym == SDLK_x)
+            speed = 0;
+          if(FLAGS_warpkeys && event.key.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_c)
+            thistick = true;
+          
           controls_key(&event.key);
           break;
 
@@ -111,91 +126,113 @@ void MainLoop() {
       break;
     if(FLAGS_terminateAfterFrame != -1 && FLAGS_terminateAfterFrame <= frameNumber)
       break;
-    interface.ai(controls_ai());  // has to be before controls
-    controllers = controls_next();
-    if(!controllers.size())
-      break;
-    CHECK(controllers.size() == origcontrollers.size());
-    for(int i = 0; i < controllers.size(); i++)
-      CHECK(controllers[i].keys.size() == origcontrollers[i].keys.size());
     {
-      Adler32 adl;
-      PerfStack pst(PBC::checksum);
-      adler(&adl, frameNumber);
-      interface.checksum(&adl);
-      reg_adler(adl);
-      adlers += ret_adler_ref_count();
-    }
-    
-    if(FLAGS_writeTarget != "" && controls_recordable()) {
-      if(frameNumber == 0) {
-        if(outfile)
-          fclose(outfile);
+      int tspeed = speed;
+      if(thistick && !tspeed)
+        tspeed = 1;
+      for(int i = 0; i < tspeed; i++) {
+        interface.ai(controls_ai());  // has to be before controls
+        controllers = controls_next();
+        if(!controllers.size())
+          break;
+        CHECK(controllers.size() == origcontrollers.size());
+        for(int i = 0; i < controllers.size(); i++)
+          CHECK(controllers[i].keys.size() == origcontrollers[i].keys.size());
+        {
+          Adler32 adl;
+          PerfStack pst(PBC::checksum);
+          adler(&adl, frameNumber);
+          interface.checksum(&adl);
+          reg_adler(adl);
+          adlers += ret_adler_ref_count();
+        }
         
-        string fname = FLAGS_writeTarget;
-        char timestampbuf[128];
-        time_t ctmt = time(NULL);
-        strftime(timestampbuf, sizeof(timestampbuf), "%Y%m%d-%H%M%S", gmtime(&ctmt));
-        fname = StringPrintf("%s-%s-%010d.dnd", fname.c_str(), timestampbuf, frameNumber);
-        dprintf("%s\n", fname.c_str());
-        outfile = fopen(fname.c_str(), "wb");
-        if(outfile) {
-          int dat = 6;
-          fwrite(&dat, 1, sizeof(dat), outfile);
-          fwrite(&game_seed, 1, sizeof(game_seed), outfile);  // this is kind of grim and nasty
-          dat = controllers.size();
-          fwrite(&dat, 1, sizeof(dat), outfile);
-          for(int i = 0; i < controllers.size(); i++) {
-            dat = controllers[i].keys.size();
-            fwrite(&dat, 1, sizeof(dat), outfile);
-            dat = controllers[i].axes.size();
-            fwrite(&dat, 1, sizeof(dat), outfile);
+        if(FLAGS_writeTarget != "" && controls_recordable()) {
+          if(frameNumber == 0) {
+            if(outfile)
+              fclose(outfile);
+            
+            string fname = FLAGS_writeTarget;
+            char timestampbuf[128];
+            time_t ctmt = time(NULL);
+            strftime(timestampbuf, sizeof(timestampbuf), "%Y%m%d-%H%M%S", gmtime(&ctmt));
+            fname = StringPrintf("%s-%s-%010d.dnd", fname.c_str(), timestampbuf, frameNumber);
+            dprintf("%s\n", fname.c_str());
+            outfile = fopen(fname.c_str(), "wb");
+            if(outfile) {
+              int dat = 6;
+              fwrite(&dat, 1, sizeof(dat), outfile);
+              fwrite(&game_seed, 1, sizeof(game_seed), outfile);  // this is kind of grim and nasty
+              dat = controllers.size();
+              fwrite(&dat, 1, sizeof(dat), outfile);
+              for(int i = 0; i < controllers.size(); i++) {
+                dat = controllers[i].keys.size();
+                fwrite(&dat, 1, sizeof(dat), outfile);
+                dat = controllers[i].axes.size();
+                fwrite(&dat, 1, sizeof(dat), outfile);
+              }
+              fflush(outfile);
+            } else {
+              dprintf("Outfile %s couldn't be opened! Not writing dump", fname.c_str());
+            }
           }
-          fflush(outfile);
-        } else {
-          dprintf("Outfile %s couldn't be opened! Not writing dump", fname.c_str());
+          if(outfile) {
+            for(int i = 0; i < controllers.size(); i++) {
+              fwrite(&controllers[i].menu.x, 1, sizeof(controllers[i].menu.x), outfile);
+              fwrite(&controllers[i].menu.y, 1, sizeof(controllers[i].menu.y), outfile);
+              fwrite(&controllers[i].u.down, 1, sizeof(controllers[i].u.down), outfile);
+              fwrite(&controllers[i].d.down, 1, sizeof(controllers[i].d.down), outfile);
+              fwrite(&controllers[i].l.down, 1, sizeof(controllers[i].l.down), outfile);
+              fwrite(&controllers[i].r.down, 1, sizeof(controllers[i].r.down), outfile);
+              for(int j = 0; j < controllers[i].keys.size(); j++)
+                fwrite(&controllers[i].keys[j].down, 1, sizeof(controllers[i].keys[j].down), outfile);
+              for(int j = 0; j < controllers[i].axes.size(); j++)
+                fwrite(&controllers[i].axes[j], 1, sizeof(controllers[i].axes[j]), outfile);
+            }
+    
+            int refc = ret_adler_ref_count();
+            fwrite(&refc, 1, sizeof(refc), outfile);
+            for(int i = 0; i < refc; i++) {
+              unsigned long ref = ret_adler_ref();
+              fwrite(&ref, 1, sizeof(ref), outfile);
+            }
+            fflush(outfile);
+          }
         }
-      }
-      if(outfile) {
-        for(int i = 0; i < controllers.size(); i++) {
-          fwrite(&controllers[i].menu.x, 1, sizeof(controllers[i].menu.x), outfile);
-          fwrite(&controllers[i].menu.y, 1, sizeof(controllers[i].menu.y), outfile);
-          fwrite(&controllers[i].u.down, 1, sizeof(controllers[i].u.down), outfile);
-          fwrite(&controllers[i].d.down, 1, sizeof(controllers[i].d.down), outfile);
-          fwrite(&controllers[i].l.down, 1, sizeof(controllers[i].l.down), outfile);
-          fwrite(&controllers[i].r.down, 1, sizeof(controllers[i].r.down), outfile);
-          for(int j = 0; j < controllers[i].keys.size(); j++)
-            fwrite(&controllers[i].keys[j].down, 1, sizeof(controllers[i].keys[j].down), outfile);
-          for(int j = 0; j < controllers[i].axes.size(); j++)
-            fwrite(&controllers[i].axes[j], 1, sizeof(controllers[i].axes[j]), outfile);
-        }
-
-        int refc = ret_adler_ref_count();
-        fwrite(&refc, 1, sizeof(refc), outfile);
-        for(int i = 0; i < refc; i++) {
-          unsigned long ref = ret_adler_ref();
-          fwrite(&ref, 1, sizeof(ref), outfile);
-        }
+        
         ret_adler_ref_clear();
-        fflush(outfile);
+        
+        controls_snag_next_checksum_set();
+        reg_adler_ul(0);  // so we have one item, and for rechecking's sake
+        
+        if(FLAGS_timing) {
+          polling += bencher.ticksElapsed();
+          bencher = Timer();
+        }
+        {
+          PerfStack pst(PBC::tick);
+          if(interface.tick(controllers, game_seed))
+            quit = true;
+        }
+        if(FLAGS_timing) {
+          ticking += bencher.ticksElapsed();
+          bencher = Timer();
+        }
+        
+        adlers += ret_adler_ref_count();
+        if(outfile) {
+          int refc = ret_adler_ref_count();
+          fwrite(&refc, 1, sizeof(refc), outfile);
+          for(int i = 0; i < refc; i++) {
+            unsigned long ref = ret_adler_ref();
+            fwrite(&ref, 1, sizeof(ref), outfile);
+          }
+          ret_adler_ref_clear();
+          fflush(outfile);
+        }
+        
+        frameNumber++;
       }
-    }
-    
-    controls_snag_next_checksum_set();
-    reg_adler_ul(0);  // so we have one item, and for rechecking's sake
-    
-    if(FLAGS_timing) {
-      polling += bencher.ticksElapsed();
-      bencher = Timer();
-    }
-    {
-      PerfStack pst(PBC::tick);
-      if(interface.tick(controllers, game_seed))
-        quit = true;
-    }
-    if(FLAGS_timing) {
-      ticking += bencher.ticksElapsed();
-      bencher = Timer();
     }
     if(FLAGS_render) {
       bool render = false;
@@ -207,6 +244,10 @@ void MainLoop() {
       } else {
         render = (frameNumber % 6 == 0) || !timer.skipFrame();
       }
+      
+      if(speed != 1)
+        render = true;
+      
       if(render) {
         {
           PerfStack pst(PBC::render);
@@ -227,18 +268,6 @@ void MainLoop() {
       } else {
         skipped++;
       }
-    }
-    
-    adlers += ret_adler_ref_count();
-    if(outfile) {
-      int refc = ret_adler_ref_count();
-      fwrite(&refc, 1, sizeof(refc), outfile);
-      for(int i = 0; i < refc; i++) {
-        unsigned long ref = ret_adler_ref();
-        fwrite(&ref, 1, sizeof(ref), outfile);
-      }
-      ret_adler_ref_clear();
-      fflush(outfile);
     }
     
     if(FLAGS_timing) {
@@ -279,7 +308,6 @@ void MainLoop() {
         adlers = 0;
       }
     }
-    frameNumber++;
     frako++;
   }
   dprintf("Control shutdown\n");
